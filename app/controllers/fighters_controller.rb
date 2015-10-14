129ee05199a21a37f76883fd183dbef452d8cfd6
Mac <LordWilliam1234@gmail.com>
2015-10-13 22:10:36 -0400

    Add every character's page, add download link

--- app/controllers/fighters_controller.rb
+++ app/controllers/fighters_controller.rb
@@ -22,4 +22,72 @@ class FightersController < ApplicationController
 
   def ryo
   end
+
+  def haggar
+  end
+
+  def terry
+  end
+
+  def makoto
+  end
+
+  def jin
+  end
+
+  def sakura
+  end
+
+  def kazuya
+  end
+
+  def karin
+  end
+
+  def haohmaru
+  end
+  
+  def dan
+  end
+
+  def hibiki
+  end
+
+  def megaman
+  end
+ 
+  def klonoa
+  end
+
+  def chun
+  end
+
+  def joe
+  end
+
+  def dudley
+  end
+
+  def lucario
+  end
+
+  def yang
+  end
+
+  def blaziken
+  end
+
+  def yun
+  end
+
+  def mario
+  end
+
+  def jill
+  end
+
+  def yoshi
+  end
+
+  
 end

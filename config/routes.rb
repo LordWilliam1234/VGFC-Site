071eff41948985871c7dca2c6e68b0c6e2dd3caa
Mac <LordWilliam1234@gmail.com>
2016-01-19 10:43:20 -0500

    Attempting to fix Heroku bug on Juri's page

--- config/routes.rb
+++ config/routes.rb
@@ -66,7 +66,7 @@ Rails.application.routes.draw do
   get 'ingrid' => 'fighters#ingrid'
   get 'kyosuke' => 'fighters#kyosuke'
   get 'fluttershy' => 'fighters#fluttershy'
-  get 'juri' => 'fighters#juri'
+  get 'jurihan' => 'fighters#jurihan'
   get 'rainbow' => 'fighters#rainbow'
   get 'king' => 'fighters#king'
   get 'decapre' => 'fighters#decapre'
